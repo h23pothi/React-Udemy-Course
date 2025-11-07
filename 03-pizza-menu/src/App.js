c31faceecbaa2856a9e8b9848eb1288403ed6503
h23pothi <h23pothi@du.se>
2025-11-07 01:52:26 +0100

    Folder (Added from my local project folder)

---
+++ 03-pizza-menu/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>Wooohooo</p>
+    </div>
+  );
+}
+
+export default App;
